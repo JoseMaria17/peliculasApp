19a572213201f2482cc28199a296ea5deb546c50
José María  González <zljosewark1a@gmail.com>
2023-02-22 14:55:56 -0500

    second commit

--- src/routes/actors.js
+++ src/routes/actors.js
@@ -16,4 +16,50 @@ router.post('/actors', (req, res) => {
 
 });
 
+
+//get all actors
+router.get('/actors', (req, res) => {
+    actorsSchema
+    .find()
+    .then((data) => res.json(data))
+    .catch((error) => res.json({message:error}));
+
+});
+
+
+//get a actor
+router.get("/actors/:id", (req, res) => {
+    const { id } = req.params;
+    actorsSchema
+      .findOne({ act_id: id })
+      .then((data) => res.json(data))
+      .catch((error) => res.json({ message: error }));
+  });
+
+
+ //update a actor
+router.put('/actors/:id', (req, res) => {
+    const { id } = req.params;
+    const {act_id, act_fname, act_lname, act_gender } = req.body;
+    actorsSchema
+    .updateOne({act_id:id}, { $set: {act_id, act_fname, act_lname, act_gender} })
+    .then((data) => res.json(data))
+    .catch((error) => res.json({message:error}));
+
+}); 
+
+
+//Delete a actor
+router.delete('/actors/:id', (req, res) =>{
+    const { id } = req.params;
+    actorsSchema
+    .remove({act_id: id})
+    .then((data) => res.json(data))
+    .catch((error) => res.json({message:error}));
+}); 
+
+
+
+
+
 module.exports = router;
